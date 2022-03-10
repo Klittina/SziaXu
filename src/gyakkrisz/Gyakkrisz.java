7c0e272254575886945828616eea6d23d45cf6c1
Xu Jiyu <matexu97@gmail.com>
2022-03-10 13:09:38 +0100

    Xu neve

--- src/gyakkrisz/Gyakkrisz.java
+++ src/gyakkrisz/Gyakkrisz.java
@@ -3,7 +3,8 @@ package gyakkrisz;
 public class Gyakkrisz {
 
     public static void main(String[] args) {
-        String nev1, nev2, nev3;
+        String nev1 ="Xu Jiyu";
+        String nev2;
     }
     
 }
