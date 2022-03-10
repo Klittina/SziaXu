4b60edda91140746ca45e87535e91a9ca26db878
Klittina <krisztike113@gmail.com>
2022-03-10 13:09:20 +0100

    nev1 deklarálása

--- src/gyakkrisz/Gyakkrisz.java
+++ src/gyakkrisz/Gyakkrisz.java
@@ -3,7 +3,7 @@ package gyakkrisz;
 public class Gyakkrisz {
 
     public static void main(String[] args) {
-        String nev1;
+        String nev1 = "Kriszti <3";
         String nev2;
     }
     
